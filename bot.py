91aa47c28e5864752fecf30ff205565398b7ab47
urasarpaev9 <urasarpaev9@gmail.com>
2026-01-17 14:40:40 +0300

    fix: исправлен токен

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 ﻿import os
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 from telegram.ext import Application, CommandHandler, ContextTypes
+
+# Получаем токен из переменной окружения с именем "BOT_TOKEN"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не задан в переменных окружения!")
 
 async def start(update, context):
     await update.message.reply_text("✅ Бот работает!")
 
 def main():
-    app = Application.builder().token(os.getenv("8544508419:AAEcYmgyx-jd1hgiVWrbiryGMMRBGfmhBAQ")).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
