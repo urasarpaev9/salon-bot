f07c41c79f2fa9785a31e0d0c26025b67a518e4f
urasarpaev9 <urasarpaev9@gmail.com>
2026-01-17 14:13:09 +0300

    Обновил бота и добавил README

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ async def start(update, context):
     await update.message.reply_text("✅ Бот работает!")
 
 def main():
-    app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
+    app = Application.builder().token(os.getenv("8544508419:AAEcYmgyx-jd1hgiVWrbiryGMMRBGfmhBAQ")).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
